a4ef7730fad37cc72b00effcd763b03d036712e3
ruiwang <Rey@xuhongs-MacBook-Pro.local>
2018-04-13 13:13:33 -0700

    enter the commit message for your changes. Lines starting

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 
-import posts from './posts';
-import comments from './comments';
 
 const rootReducer = combineReducers({
-    posts,
-    comments,
     firebase: firebaseReducer
 });
                                                         //the changing of the URL
